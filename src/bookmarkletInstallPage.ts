5cf50a6ba1823d2247325783474506374c1a0fd0
Colby <colby.leiske@gmail.com>
2024-08-24 18:59:23 -0700

    clean up comment

--- src/bookmarkletInstallPage.ts
+++ src/bookmarkletInstallPage.ts
@@ -2,7 +2,6 @@ import Handlebars from "handlebars";
 import InstallPageTemplate from './installPage.hbs' with { type: "text" };
 
 export async function buildBookmarkletInstallPage(bookmarklet: string, hotReloadBookmarklet: string): Promise<string> {
-  // const templateRaw = await Bun.file('./src/installPage.hbs').text();
   const template = Handlebars.compile(InstallPageTemplate);
   return template({
     bookmarklet,
