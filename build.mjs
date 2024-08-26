e25e2e112229bcb14e107e44dba5c9f5e0f8f075
Colby <colby.leiske@gmail.com>
2024-08-25 23:40:31 -0700

    rework the entrypoint and build naming scheme to prevent the src fodler cascage in there. Increment again,lol. The types arent workign when consuming the package yet though which is odd

--- build.mjs
+++ build.mjs
@@ -1,8 +1,10 @@
 import dts from 'bun-plugin-dts'
 
 await Bun.build({
-  entrypoints: ['./index.ts'],
+  entrypoints: ['./index.ts', './src/helpers/helpers.js'],
+  naming: '[name].[ext]',
   outdir: './dist',
   minify: true,
+  sourcemap: "linked",
   plugins: [dts()]
 })
